aa26dc142671a7ca6cca39da158ee473bf4280c6
sarterni <sarter.nicolas@gmail.com>
2024-06-28 10:21:58 +0200

    secure

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+import { API_KEY } from './config.js';
 function updateWeather(ville) {
-    const apiKey = "414f550b460063e8d5f1216f6e0c041a";
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${API_KEY}`)
         .then(response => response.json())
         .then(data => {
             document.getElementById("ville-name").textContent = data.name; // Display city name
